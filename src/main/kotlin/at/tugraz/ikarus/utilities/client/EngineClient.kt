45dcd14d83e25bb75c19ef21434751d92000f920
Donat <donutellko@gmail.com>
2019-04-06 19:47:34 +0300

    Updated the classes to match the updated wsdl and added them to sources as well

--- src/main/kotlin/at/tugraz/ikarus/utilities/client/EngineClient.kt
+++ src/main/kotlin/at/tugraz/ikarus/utilities/client/EngineClient.kt
@@ -1,22 +1,36 @@
 package at.tugraz.ikarus.utilities.client
 
+import at.tugraz.ikarus.engine.*
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport
-
-import CoreWS.wsdl.GET
-import CoreWS.wsdl.Hello
-import CoreWS.wsdl.STORE
+import javax.xml.bind.JAXBElement
 
 // https://spring.io/guides/gs/consuming-web-service/
+@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 class EngineClient : WebServiceGatewaySupport() {
 
-    fun hello(name: String) =
-        webServiceTemplate.marshalSendAndReceive(Hello().apply { this.name = name })
+    /**
+     * Marshal and send the given object, receive
+     * */
+    private fun send(request: Any): JAXBElement<*> =
+        webServiceTemplate.marshalSendAndReceive(request) as JAXBElement<*>
 
-    fun store(content: String) =
-        webServiceTemplate.marshalSendAndReceive(STORE().apply { this.content = content })
+    fun hello(name: String): HelloResponse {
+        val response = send(Hello().apply { this.name = name })
+        return response.value as HelloResponse
+    }
 
-    fun get(id: String) =
-        webServiceTemplate.marshalSendAndReceive(GET().apply { this.id = id })
+    fun store(content: String): StoreResponse {
+        val response = send(Store().apply { this.content = content })
+        return response.value as StoreResponse
+    }
 
-    fun delete(id: String): String? = null
+    fun get(id: String): GetResponse {
+        val response = send(Get().apply { this.id = id })
+        return response.value as GetResponse
+    }
+
+    fun delete(id: String): DeleteResponse {
+        val response = send(Delete().apply { this.id = id })
+        return response.value as DeleteResponse
+    }
 }
